2955b2a91dbbb5acf1f5da9a627b5b69006e1978
Ruslan Gritsenko <gritsenko.ruslan@gmail.com>
2018-09-22 17:20:57 +0300

    Added Strings.swift

---
+++ 25/Supporting Files/Strings.swift
@@ -0,0 +1,24 @@
+//
+//  Strings.swift
+//  25
+//
+//  Created by Ruslan Gritsenko on 22.09.2018.
+//  Copyright © 2018 Ruslan Gritsenko. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Notifications
+
+let DarkModeStateDidChangeNotification = "DarkModeStateDidChangeNotification"
+let DarkModeStateUserInfoKey = "DarkModeStateUserInfoKey"
+
+let UserInterfaceColorDidChangeNotification = "UserInterfaceColorDidChangeNotification"
+let UserInterfaceColorUserInfoKey = "UserInterfaceColorUserInfoKey"
+
+// MARK: - User Defaults
+
+let DarkModeUserDefaultsKey = "darkMode"
+let AutomaticDarkModeUserDefaultsKey = "automaticDarkModeKey"
+let SunriseUserDefaultsKey = "sunrise"
+let SunsetUserDefaultsKey = "sunset"
